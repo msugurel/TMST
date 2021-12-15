5e12855a61e271c030a664a6e2aa2d7d84d23ac6
FIRAT <dr.ugurel@gmail.com>
2021-12-15 13:06:41 +0300

    Material reducer oluşturuldu ve kodlandı.

---
+++ tmst/src/reducers/materialReducer.js
@@ -0,0 +1,65 @@
+//Created by [rxreducer] snippet
+
+import {
+    ADD_MATERIAL_FULFILLED, ADD_MATERIAL_PENDING, ADD_MATERIAL_REJECTED,
+    GET_MATERIAL_FULFILLED, GET_MATERIAL_PENDING, GET_MATERIAL_REJECTED,
+    UPDATE_MATERIAL_FULFILLED, UPDATE_MATERIAL_PENDING, UPDATE_MATERIAL_REJECTED,
+    DELETE_MATERIAL_FULFILLED, DELETE_MATERIAL_PENDING, DELETE_MATERIAL_REJECTED,
+    FETCH_MATERIAL_FULFILLED, FETCH_MATERIAL_PENDING, FETCH_MATERIAL_REJECTED
+} from "../actions/materialActions";
+
+const initialState = {
+    material: [], error: {}, loading: false, done: false, gotMaterial: {}
+}
+
+export default (state = initialState, { type, payload }) => {
+    switch (type) {
+        //type==ADD_MATERIAL
+        case ADD_MATERIAL_PENDING:
+            return { ...state, loading: true, done: false }
+        case ADD_MATERIAL_FULFILLED:
+            return { ...state, material: payload, loading: false, done: true }
+        case ADD_MATERIAL_REJECTED:
+            return { ...state, error: payload, loading: false, done: false }
+
+        //type==UPDATE_MATERIAL
+        case UPDATE_MATERIAL_PENDING:
+            return { ...state, loading: true, done: false }
+        case UPDATE_MATERIAL_FULFILLED:
+            return { ...state, material: payload, loading: false, done: true }
+        case UPDATE_MATERIAL_REJECTED:
+            return { ...state, error: payload, loading: false, done: false }
+
+        //type== GET_MATERIAL 
+        case GET_MATERIAL_PENDING:
+            return { ...state, loading: true, done: false }
+        case GET_MATERIAL_FULFILLED:
+            return { ...state, gotMaterial: payload, loading: false, done: true }
+        case GET_MATERIAL_REJECTED:
+            return { ...state, error: payload, loading: false, done: false }
+
+        //type== FETCH_MATERIAL 
+        case FETCH_MATERIAL_PENDING:
+            return { ...state, loading: true }
+        case FETCH_MATERIAL_FULFILLED:
+            return { ...state, material: payload, loading: false }
+        case FETCH_MATERIAL_REJECTED:
+            return { ...state, error: payload, loading: false }
+
+        //type== DELETE_MATERIAL 
+        case DELETE_MATERIAL_PENDING:
+            return { ...state, loading: true, done: false }
+        case DELETE_MATERIAL_FULFILLED:
+            //console.log("From Action=>",payload.id);
+            //console.log("From State=>",state.material);
+            return {
+                ...state
+                , material: state.material.filter(i => i.id !== payload.id)
+                , loading: false, done: true
+            }
+        case DELETE_MATERIAL_REJECTED:
+            return { ...state, error: payload, loading: false, done: false }
+        default:
+            return state
+    }
+}
