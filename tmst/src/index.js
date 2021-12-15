d9f8c94e36527ca953eb43d03e8c2bcbcee5b59b
FIRAT <dr.ugurel@gmail.com>
2021-12-15 12:54:28 +0300

    Eklenen bütün kütüphanele ve middleware ler ve store yapısı index.js de eklendi.
    Eklenen bütün kütüphanele ve middleware ler ve store yapısı index.js de eklendi.

--- tmst/src/index.js
+++ tmst/src/index.js
@@ -4,15 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'semantic-ui-css/semantic.min.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter,Router } from 'react-router-dom';
+import { createStore,applyMiddleware } from "redux";
+import rootReducer from './reducers/rootReducer'
+import thunk from 'redux-thunk'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { Provider } from "react-redux";
+import logger from 'redux-logger';
+import promise from 'redux-promise-middleware'
+
+const store = createStore(rootReducer,
+  composeWithDevTools(applyMiddleware(thunk,promise,logger)))
 
 ReactDOM.render(
- 
+  // <React.StrictMode>
     <BrowserRouter>
-      <App />
-    </BrowserRouter>,
-  
-
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  // </React.StrictMode>
+  ,
   document.getElementById('root')
 );
 
