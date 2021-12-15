3acfb15138ba76597a7437993c3a34093b03870a
FIRAT <dr.ugurel@gmail.com>
2021-12-15 14:13:58 +0300

    Daha özellikli bir datatable eklendi : npm install react-data-table-component
    Daha özellikli bir datatable eklendi : npm install react-data-table-component

--- tmst/src/components/pages/NewMaterialPage.js
+++ tmst/src/components/pages/NewMaterialPage.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import { Container } from 'semantic-ui-react'
+//Created by [rcredux] snippet
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import NewMaterialForm from '../NewMaterialForm'
+import { addNewMaterial,getMaterial,updateMaterial } from '../../actions/materialActions';
 
-export default function NewMaterialPage() {
-    return (
-        <div>
-          <Container>
-              Yeni Malzeme
-              Yeni Malzeme
-              Yeni Malzeme
-              Yeni Malzeme
-              Yeni Malzeme
-          </Container>
-        </div>
-    )
+export class NewMaterialPage extends Component {
+    componentDidMount() {
+       const {match} = this.props;
+       if (!this.props.material && match.params.id) {
+           this.props.getMaterial(match.params.id);
+       } 
+    }
+    
+    
+    render() {
+        return (
+          <div>
+            Add New Material Page
+            <NewMaterialForm
+              done={this.props.materialReducer.done}
+              errorText={this.props.materialReducer.error}
+              loading={this.props.materialReducer.loading}
+              addNewMaterial={this.props.addNewMaterial}
+              updateMaterial={this.props.updateMaterial}
+              material={this.props.material}
+              gotMaterial={this.props.materialReducer.gotMaterial}
+            />
+          </div>
+        );
+    }
 }
+const mapStateToProps = ({materialReducer},props) => {
+  console.log("ddd",materialReducer)
+return {
+    materialReducer:materialReducer,
+    material:materialReducer.material.data
+    /*TODO:::
+.find(item=>item._id === props.match.params.id)*/}
+}
+
+const mapDispatchToProps = {
+        addNewMaterial,getMaterial,updateMaterial
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(NewMaterialPage)
