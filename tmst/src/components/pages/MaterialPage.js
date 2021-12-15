6bafdbe798ab76dd92a80c0b9adb2e174cd636fc
FIRAT <dr.ugurel@gmail.com>
2021-12-15 14:12:32 +0300

    Material eklemek için gereken NewMaterialPage sayfası kodlandı.
    Yeni material eklemek için gereken NewMaterialPage sayfası kodlandı.

--- tmst/src/components/pages/MaterialPage.js
+++ tmst/src/components/pages/MaterialPage.js
@@ -1,57 +1,78 @@
 
-import { Container } from "semantic-ui-react";
+import { Container, Segment, Button, Icon } from "semantic-ui-react";
 import DataTablesComp from "../DataTables";
 import axios from 'axios';
 import React, { useState, useEffect } from 'react'
+import { fetchMaterials, deleteMaterial } from '../../actions/materialActions';
+import { connect } from 'react-redux'
+import DataTable from 'react-data-table-component';
 
-export default function MaterialPage() {
+export const MaterialPage = ({ deleteMaterial, fetchMaterials, materialReducer }) => {
     const [tableData, setTableData] = useState(null)
     const deleteRow = (id) => {
-        const filteredData = this.state.data.filter((i) => i.id !== id);
-        this.setState({ data: filteredData });
+        deleteMaterial(id)
+        setTimeout(() => {
+            fetchMaterials();
+        }, 500);
+
+        console.log(materialReducer)
     };
 
-    const getDataFromApi = () => {
-        axios.get('http://localhost:5000/materials')
-            .then(function (response) {
-                // handle success
-                var newData = [];
-                response.data.map((item, index) => {
-                    var newItem = {
-                        name: item.Name, id: item._id,
-                        position: "System Architect",
-                        office: "Edinburgh",
-                        ext: 5421,
-                        date: "2011/04/25",
-                        salary: "$320,800",
-                    }
-                    newData.push(newItem);
-                })
-                console.log("data", newData)
-                setTableData(newData)
-            })
-            .catch(function (error) {
-                // handle error
-                console.log(error);
-            })
-            .then(function () {
-                // always executed
-            });
-    }
     useEffect(() => {
-        getDataFromApi();
+        fetchMaterials();
     }, [])
+
+    const columns = [
+        {
+            name: 'Malzeme Adı',
+            selector: row => row.Name,sortable: true,
+        },
+        {
+            name: 'İşlem',width:'250px',
+            cell: (row) => <>
+                <Button inverted color='red' size='mini'
+                data-toggle="modal"
+                data-target="#DeleteModal"
+                id={row._id}
+                onClick={() => { deleteRow(row._id); }}><Icon name='trash' /> Sil </Button>
+                <a className="ui mini button" href={`/malzeme/${row._id}`}><Icon name='edit' />  Düzenle</a>
+            </>,
+            ignoreRowClick: true,
+            allowOverflow: true,
+            button: true,
+        },
+    ];
+
+
     return (
-
         <div>
-            <Container>
-{
-    tableData == null ? "Veri yok" :
-    <DataTablesComp columns={5} data={tableData} deleteRow={deleteRow}  />
 
-}
+            <Container >
+                {materialReducer.loading && "Veriler Yükleniyor..."}
+                <Segment loading={materialReducer.loading}>
+                    <DataTable
+                        columns={columns}
+                        data={materialReducer.material} pagination
+                    />
+                </Segment>
             </Container>
-
         </div>
     )
 }
+
+
+
+
+const mapStateToProps = ({ materialReducer }) => ({
+    materialReducer
+})
+
+
+const mapDispatchToProps = {
+    fetchMaterials, deleteMaterial
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(MaterialPage)
+
+
+
