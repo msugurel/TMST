e815634f81ffdefbe6ad41c19da03d1d79fbb220
FIRAT <dr.ugurel@gmail.com>
2021-12-20 09:58:07 +0300

    Stok tablosunu diğer tablolarla JOIN leyip getiren route kodlandı.

--- tmst-api/routes/stocks.js
+++ tmst-api/routes/stocks.js
@@ -8,7 +8,36 @@ router.get("/", (req, res) => {
     res.json(data);
   });
 });
+router.get("/all", function (req, res, next) {
 
+  Model.aggregate([
+    
+    { $set: { MaterialId: { $toObjectId: "$MaterialId" } } },
+    { $lookup: {
+      from: "materials",
+      localField:'MaterialId',        
+      foreignField: '_id',
+      as:'Material'
+    }},
+    { $set: { 'WarehouseId': { $toObjectId: "$WarehouseId" } } },
+    { $lookup: {
+      from: "warehouses",
+      localField:'WarehouseId',        
+      foreignField: '_id',
+      as:'Warehouse'
+    }},
+    {$project:{
+      _id:1,
+      Quantity:1,
+      ProcessDate:true,
+      SKT:1,
+      CreatedAt:1,
+      'MaterialName':{ "$arrayElemAt": ["$Material.Name", 0] },
+      'WarehouseName':{ "$arrayElemAt": ["$Warehouse.Name", 0] },
+}}
+  ]).then((data) => { res.json(data); })
+    .catch((err) => { res.json(err); })
+});
 router.get("/:Id", (req, res,next) => {
   Model.findById({"_id":req.params.Id}, (err, data) => {
     if (err) res.json(err);
